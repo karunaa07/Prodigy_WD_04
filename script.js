d6152826c881cd5a4365c67483bf957f8403b721
karunaa07 <kashilkarkaruna@gmail.com>
2024-12-31 19:32:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth Scroll for Navigation Links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
